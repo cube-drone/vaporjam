44b6f51ad90669e9fa2658f8876367bedaf7b7ff
Curtis <curtis@lassam.net>
2021-06-28 01:47:51 -0700

    TitleScreen can be removed

--- src/TitleScreen.js
+++ src/TitleScreen.js
@@ -10,21 +10,47 @@ export default class TitleScreen{
         this.app = app;
         this.started = false;
         this.container = new PIXI.Container();
+
+        this.start = this.start.bind(this);
+        this.animate = this.animate.bind(this);
+        this.triggerTitleWindow = this.triggerTitleWindow.bind(this);
+        this.stop = this.stop.bind(this);
     }
 
     start(){
         let app = this.app;
-        let bg = new PIXI.Sprite(app.loader.resources.background.texture);
-        this.container.addChild(bg);
+        this.bg = new PIXI.Sprite(app.loader.resources.background.texture);
+        this.container.addChild(this.bg);
         
-        let grid = new PIXI.Sprite(app.loader.resources.bg_grid.texture);
-        this.container.addChild(grid);
+        this.grid = new PIXI.Sprite(app.loader.resources.bg_grid.texture);
+        this.container.addChild(this.grid);
 
-        let memphis = new PIXI.Sprite(app.loader.resources.bg_memphis.texture);
-        memphis.scale.x = 1.5;
-        memphis.scale.y = 1.5;
-        this.container.addChild(memphis);
+        this.memphis = new PIXI.Sprite(app.loader.resources.bg_memphis.texture);
+        this.memphis.scale.x = 1.5;
+        this.memphis.scale.y = 1.5;
+        this.container.addChild(this.memphis);
         
+        app.stage.addChild(this.container);
+        
+        setTimeout(this.triggerTitleWindow, 3000);
+
+        app.ticker.add(this.animate);
+    }
+
+    animate(){
+        this.memphis.x -= 1;
+        this.grid.y -= 1;
+        if(this.memphis.x < -600){
+            this.memphis.x = 0;
+        }
+        if(this.grid.y < -500){
+            this.grid.y = 0;
+        }
+    }
+
+    triggerTitleWindow(){
+        let app = this.app;
+
         let titleWindow = new PIXI.Sprite(app.loader.resources.title_window.texture);
         titleWindow.x = 10;
         titleWindow.y = 10;
@@ -34,23 +60,12 @@ export default class TitleScreen{
         title.x = 240;
         title.y = 130;
         this.container.addChild(title);
-
-        app.stage.addChild(this.container);
-        
-        app.ticker.add((dt)=>{
-            memphis.x -= 1;
-            grid.y -= 1;
-            if(memphis.x < -600){
-                memphis.x = 0;
-            }
-            if(grid.y < -500){
-                grid.y = 0;
-            }
-        })
     }
 
     stop(){
-
+        this.app.stage.removeChild(this.container);
+        this.container.destroy();
+        this.app.ticker.remove(this.animate);
     }
 
 }
